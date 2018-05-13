a7ee786aee8b296c6e1ab65c30376258af423256
tarurar <tarutin_andrew@mail.ru>
2018-05-13 20:23:04 +0300

    added auto registration in monitoring

--- src/Lykke.Service.PaySign/Startup.cs
+++ src/Lykke.Service.PaySign/Startup.cs
@@ -7,6 +7,10 @@ using Common.Log;
 using Lykke.Common.ApiLibrary.Middleware;
 using Lykke.Common.ApiLibrary.Swagger;
 using Lykke.Logs;
+
+// ReSharper disable once RedundantUsingDirective
+using Lykke.MonitoringServiceApiCaller;
+
 using Lykke.Service.PaySign.Core.Services;
 using Lykke.Service.PaySign.Core.Settings;
 using Lykke.Service.PaySign.Filters;
@@ -27,6 +31,9 @@ namespace Lykke.Service.PaySign
         public IContainer ApplicationContainer { get; private set; }
         public IConfigurationRoot Configuration { get; }
         public ILog Log { get; private set; }
+
+        // ReSharper disable once NotAccessedField.Local
+        private string _monitoringServiceUrl;
 
         public Startup(IHostingEnvironment env)
         {
@@ -60,6 +67,7 @@ namespace Lykke.Service.PaySign
 
                 var builder = new ContainerBuilder();
                 var appSettings = Configuration.LoadSettings<AppSettings>();
+                _monitoringServiceUrl = appSettings.CurrentValue.MonitoringServiceClient?.MonitoringServiceUrl;
 
                 Log = CreateLogWithSlack(services, appSettings);
 
@@ -119,6 +127,10 @@ namespace Lykke.Service.PaySign
                 await ApplicationContainer.Resolve<IStartupManager>().StartAsync();
 
                 await Log.WriteMonitorAsync("", $"Env: {Program.EnvInfo}", "Started");
+#if !DEBUG
+                if (!string.IsNullOrEmpty(_monitoringServiceUrl))
+                    await AutoRegistrationInMonitoring.RegisterAsync(Configuration, _monitoringServiceUrl, Log);
+#endif
             }
             catch (Exception ex)
             {
